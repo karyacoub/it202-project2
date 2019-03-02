2c6891e6ef2adb389714548919a1b08110f20d30
Kareem Yacoub <kyacou2@uic.edu>
2019-03-02 13:48:45 -0600

    Revert "Revert "changed map size and zoom level""
    This reverts commit c91cc61ad4b854399277dbf30852290fd930d7b7.

--- map.js
+++ map.js
@@ -4,6 +4,6 @@ function initMap()
 {
     map = new google.maps.Map(document.getElementById('map-div'), {
         center: { lat: 41.8781, lng: -87.6298 },
-        zoom: 12
+        zoom: 10
     });
 }
